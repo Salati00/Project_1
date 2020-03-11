c8179cd31cead42be3125972d2bd16e9cb715259
Giorgio <salati00.giorgio@gmail.com>
2020-03-11 19:31:48 +0100

    Mostly done W2A, Fixed Connection

--- Moist_UwU/SomerenDAL/Student_DAO.cs
+++ Moist_UwU/SomerenDAL/Student_DAO.cs
@@ -15,7 +15,7 @@ namespace SomerenDAL
       
         public List<Student> Db_Get_All_Students()
         {
-            string query = "SELECT student_id, student_name FROM [TABLE]";
+            string query = "SELECT student_id, first_name FROM [student]";
             SqlParameter[] sqlParameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
@@ -29,7 +29,7 @@ namespace SomerenDAL
                 Student student = new Student()
                 {
                     Number = (int)dr["student_id"],
-                    Name = (String)(dr["student_name"].ToString())
+                    Name = (String)(dr["first_name"].ToString())
                 };
                 students.Add(student);
             }
